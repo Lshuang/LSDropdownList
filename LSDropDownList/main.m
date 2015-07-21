0f8bc0e45c827487a1edf9f894395206ef522f70
LShawn <730356947@qq.com>
2015-07-21 17:19:13 +0800

    Initial Commit

---
+++ LSDropDownList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LSDropDownList
+//
+//  Created by Shawn Li on 15/7/21.
+//  Copyright (c) 2015年 Shawn Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ LSDropDownListTests/LSDropDownListTests.m
@@ -0,0 +1,40 @@
+//
+//  LSDropDownListTests.m
+//  LSDropDownListTests
+//
+//  Created by Shawn Li on 15/7/21.
+//  Copyright (c) 2015年 Shawn Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LSDropDownListTests : XCTestCase
+
+@end
+
+@implementation LSDropDownListTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
